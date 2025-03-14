b7a4883d734f2b1615a811020c0f58e1232c7aa1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-14 11:28:04 -0300

    Add entity pagamento, application-teste, update pom, update application.properties

---
+++ ms-pagamento/src/main/java/com/github/yasminmiguez/ms_pagamento/entity/Pagamento.java
@@ -0,0 +1,36 @@
+package com.github.yasminmiguez.ms_pagamento.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_pagamento")
+
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    private String nome;
+    private String numeroDoCartao;
+    private String validade;
+    private String codigoDeSeguranca;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+    @Column(nullable = false)
+    private Long pedidoId;
+    @Column(nullable = false)
+    private Long formaDePagamentoId;
+
+
+}
---
+++ ms-pagamento/src/main/java/com/github/yasminmiguez/ms_pagamento/entity/Status.java
@@ -0,0 +1,7 @@
+package com.github.yasminmiguez.ms_pagamento.entity;
+
+public enum Status {
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
