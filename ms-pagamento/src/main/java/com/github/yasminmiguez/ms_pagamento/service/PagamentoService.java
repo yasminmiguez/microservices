f3db945f164cb27321d154590a6dcca403436839
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 09:40:30 -0300

    add getAll no pagamentoService

---
+++ ms-pagamento/src/main/java/com/github/yasminmiguez/ms_pagamento/service/PagamentoService.java
@@ -0,0 +1,27 @@
+package com.github.yasminmiguez.ms_pagamento.service;
+
+import com.github.yasminmiguez.ms_pagamento.dto.PagamentoDTO;
+import com.github.yasminmiguez.ms_pagamento.entity.Pagamento;
+import com.github.yasminmiguez.ms_pagamento.repository.PagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PagamentoService {
+
+
+    @Autowired
+    private PagamentoRepository repository;
+
+@Transactional(readOnly = true)
+    public List<PagamentoDTO> getAll(){
+        List<Pagamento> pagamentos = repository.findAll();
+        return pagamentos.stream().map(PagamentoDTO::new).collect(Collectors.toList());
+
+    }
+
+}
