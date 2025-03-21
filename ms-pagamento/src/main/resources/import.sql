0e0052e40309a428aec5e53cabf5800054b0ad80
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 08:55:16 -0300

    add carga db

---
+++ ms-pagamento/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+ INSERT INTO tb_pagamento(valor, nome,numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id)  values(35.25, 'Jon Snow', '123412341234', '12/30','123','CRIADO', 1,2);
+ INSERT INTO tb_pagamento(valor, nome,numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id)  values(125.32, 'Cida', '126411391238', '08/32','568','CRIADO', 2,2);
+ INSERT INTO tb_pagamento(valor, nome,numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id)  values(75.0, 'Maria Joaquina', '840415391238', '03/32','985','CRIADO', 3,2);
+
+
+
+
